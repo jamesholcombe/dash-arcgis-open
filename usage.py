a5e200e27ceea78e41399c929894ff3176ed82a4
jamesholcombe <jholcombe@hotmail.co.uk>
2022-06-08 22:53:43 +0100

    remove url

--- usage.py
+++ usage.py
@@ -4,10 +4,7 @@ import dash_html_components as html
 import dash_core_components as dcc
 import dash
 
-import dash_arcgis_open as dago
 
-
-dago.GeoJSONLayer
 
 SHEETS = ["https://js.arcgis.com/4.23/esri/themes/light/main.css"]
 
@@ -26,7 +23,7 @@ app.layout = html.Div(
                     children=[
                         GeoJSONLayer(
                             id="geojson-layer",
-                            url="https://celh.blob.core.windows.net/germany/small_data.json?sp=r&st=2022-06-07T18:47:07Z&se=2022-07-09T02:47:07Z&sv=2020-08-04&sr=b&sig=5JskYj504vGm9tZAREPXxb5FdLbFhzchCm40ht1ef1E%3D",
+                            url="",
                              
                         )
                     ],
